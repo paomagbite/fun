88f065bd46920a931eb5198995807f0ce936ce55
Dbeela <paomagbite@gmail.com>
2021-10-28 16:38:29 +0800

    This is my fun directory

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test.h"
+
+int main () {
+    testFunc();
+
+    return 0;
+}
---
+++ src/test.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+
+void testFunc() {
+    std::cout << "Hello from test.h!" << std::endl;
+}
+
+#endif //TEST_H
